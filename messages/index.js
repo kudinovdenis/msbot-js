32a3428aa869e57e1b29a25c2cf1e00498fbcb05
Denis Kudinov <Denis.Kudinov@acronis.com>
2017-04-13 20:04:21 +0300

    qa bot

--- messages/index.js
+++ messages/index.js
@@ -13,9 +13,51 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 
 var bot = new builder.UniversalBot(connector);
 
-bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
-});
+var luisURL = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/c73182a6-cd96-43e2-b680-07b7aa43c73b?subscription-key=1302fa19ca8d4d80bad455fc33201687&timezoneOffset=0.0&verbose=true"
+var luisRecognizer = new builder.LuisRecognizer(luisURL);
+var intents = new builder.IntentDialog({ recognizers: [luisRecognizer] });
+bot.dialog('/', intents);
+
+intents.matches('SearchForFeaturesAndDates', [
+    function (session, args, next) {
+        var entities = args.entities;
+        entities.forEach(function(entity) {
+            console.log("entity: ", entity);
+            if (entity.entity == "photos" || entity.entity == "photo") {
+                builder.Prompts.choice(session, "You loooking for some photo. Specify date?", ["Yes", "No"])
+            }
+        }, this);
+    },
+    function (session, results) {
+        if (results.response) {
+            if (results.response.entity == "Yes") {
+                session.send("You answered yes. Will search with dates");
+                builder.Prompts.time(session, "choose date");
+            }
+            else {
+                builder.Prompts.text(session, "You answered no. No dates...");
+            }
+        }
+    }, 
+    function (session, results) {
+        console.log("Date result: ", results);
+        session.send(results);
+    }
+]);
+
+// bot.dialog('/', function (session) {
+//     session.send('You said ' + session.message.text);
+
+//     var luisURL = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/c73182a6-cd96-43e2-b680-07b7aa43c73b?subscription-key=1302fa19ca8d4d80bad455fc33201687&timezoneOffset=0.0&verbose=true"
+//     var luisRecognizer = new builder.LuisRecognizer(luisURL);
+//     console.log("recognizer: ", luisRecognizer);
+//     console.log("recognize context: ", session.toRecognizeContext);
+//     luisRecognizer.recognize(session.toRecognizeContext, (error, result) => {
+//         console.log("result: ", result);
+//         console.log("error: ", error);
+//     });
+    
+// });
 
 if (useEmulator) {
     var restify = require('restify');
@@ -23,7 +65,7 @@ if (useEmulator) {
     server.listen(3978, function() {
         console.log('test bot endpont at http://localhost:3978/api/messages');
     });
-    server.post('/api/messages', connector.listen());    
+    server.post('/api/messages', connector.listen());  
 } else {
     module.exports = { default: connector.listen() }
 }
